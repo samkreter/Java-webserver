717cfa92580fa136c91f6377016530723a2b3c2a
Sam Kreter <sakfy6@mail.missouri.edu>
2014-12-06 11:38:05 -0600

    building inti class and set up varibles

---
+++ Java-Webserver/src/server/Server_Starter.java
@@ -0,0 +1,19 @@
+package server;
+
+import javax.swing.*;
+
+
+public class Server_Starter extends JFrame{
+	
+	
+	JPanel jpanel1 = new JPanel();
+	JScrollPane jscrollPane1 = new JScrollPane();
+	JTextArea jTextArea2 = new JTextArea();
+	static Integer listen_port = null;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
