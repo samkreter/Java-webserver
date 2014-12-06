b44fe4473640bb4ee6a65161776af4ad90bc27a2
Sam Kreter <sakfy6@mail.missouri.edu>
2014-12-06 13:26:32 -0600

    handling all goes good 202 request

--- Java-Webserver/src/server/Server.java
+++ Java-Webserver/src/server/Server.java
@@ -68,7 +68,90 @@ public class Server extends Thread{
 	}
 	
 	//implementation of simple http
-	private void http_handler(){
+	private void http_handler(BufferedReader input, DataOutputStream output){
+		int method = 0; //1 get, 2 head, 0 not upported
+		String http = new String();
+		String path = new String(); //what path 
+		String file = new String(); //what file
+		String user_agent = new String();//what user agent 
+		
+		try{
+			String temp = input.readLine(); //read from the input 
+			String temp2 = new String(temp);
+			temp.toUpperCase(); //convert to upper case
+			if(temp.startsWith("GET")){
+				method = 1;
+			}
+			if(temp.startsWith("HEAD")){
+				method = 2;
+			}
+			if(method == 0){
+				try{
+					output.writeBytes(construct_http_header(501,0));
+					output.close();
+					return;
+				}catch(Exception e3){
+					s("ERROR: "+e3.getMessage());
+				}
+			}
+			
+			int start = 0;
+			int end = 0;
+			for(int a=0;a<temp2.length();a++){
+				if(temp2.charAt(a) == ' ' && start != 0){
+					end = a;
+					break;
+				}
+				if(temp2.charAt(a) == ' ' && start == 0){
+					start = a;
+				}
+			}
+			
+			path = temp2.substring(start+2,end); //fill the path 
+			
+		}catch(Exception e){
+			s("ERROR: "+e.getMessage());
+		}
+		
+		//show the request file 
+		s("\nClient requested:" + new File(path).getAbsolutePath() + "\n");
+		FileInputStream requestedFile = null;
+		
+		try{
+			//this is unsecured --- I really need to fix this later---- but this will work for now
+			requestedFile = new FileInputStream(path);
+			
+		}catch(Exception e){
+			try{
+				//if file can't be opened send a 404
+				output.writeBytes(construct_http_header(404,0));
+				//close stream
+				output.close();
+			}catch(Exception e2){}
+			;
+			s("ERROR "+e.getMessage());
+		}
+		
+		//good senerio ===========================
+		try{
+			int type_is = 0;
+			//get file ending to construct the correct type
+			if(path.endsWith(".zip")){
+				type_is = 3;
+			}
+			if(path.endsWith(".jpg") || path.endsWith(".jpeg")){
+				type_is = 1;
+			}
+			if(path.endsWith(".gif")){
+				type_is = 2;
+			}
+			
+			//create header with everything is just lovely on this side man
+			output.writeByte(construct_http_header(202,0));
+			
+		}catch(Exception e){
+			
+		}
 		
 	}
 	
